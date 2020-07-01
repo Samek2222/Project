be1e0a66deebee471e18def9b99dc797366e33c4
Samek2222 <66680258+Samek2222@users.noreply.github.com>
2020-07-01 19:39:42 +0200

    Add files via upload

---
+++ strona/PLCommunity/log.php
@@ -0,0 +1,39 @@
+<?php
+
+	session_start();
+	
+	if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true))
+	{
+		header('Location: gra.php');
+		exit();
+	}
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<link rel="stylesheet" type="text/css" href="css.css"/>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Panel Logowania</title>
+</head>
+
+<body>
+	
+	<b>Zaloguj się!</b><br /><br />
+	
+	<form action="zaloguj.php" method="post">
+	
+		Login: <br /> <input type="text" name="login" /> <br />
+		Hasło: <br /> <input type="password" name="haslo" /> <br /><br />
+		<input type="submit" value="Zaloguj się" />
+	
+	</form>
+	
+<?php
+	if(isset($_SESSION['blad']))	echo $_SESSION['blad'];
+?>
+
+</body>
+</html>
